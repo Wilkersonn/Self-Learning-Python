afc207bb86717d8a5facdf65b5f8d4c6f1f2eddd
Wilkersonn <remyfournieroff@gmail.com>
2024-08-09 17:42:01 +0200

    Added Basic_learning
    -Here are some basics code !

---
+++ Basic_learning/0-basic.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+
+# ▄▄▌ ▐ ▄▌▪  ▄▄▌  ▄ •▄ ▄▄▄ .▄▄▄  .▄▄ ·        ▐ ▄ 
+# ██· █▌▐███ ██•  █▌▄▌▪▀▄.▀·▀▄ █·▐█ ▀. ▪     •█▌▐█
+# ██▪▐█▐▐▌▐█·██▪  ▐▀▀▄·▐▀▀▪▄▐▀▀▄ ▄▀▀▀█▄ ▄█▀▄ ▐█▐▐▌
+# ▐█▌██▐█▌▐█▌▐█▌▐▌▐█.█▌▐█▄▄▌▐█•█▌▐█▄▪▐█▐█▌.▐▌██▐█▌
+#  ▀▀▀▀ ▀▪▀▀▀.▀▀▀ ·▀  ▀ ▀▀▀ .▀  ▀ ▀▀▀▀  ▀█▄▀▪▀▀ █▪
+
+# ┌────────────────────────────────────────┐
+# │                  Info                  │
+# └────────────────────────────────────────┘
+#
+# UTF-8 ➔ All languages supported   
+# \n    ➔ New line 
+# \t    ➔ Tab 
+# \     ➔ Print special char.
+
+
+print("\nUsage of '\\n' :")
+print("➔ I have\ntwo lines !\n")
+
+print("Usage of '\\t' :")
+print("\t ➔ I have tab\n")
+
+print("Usage of '\\' :")
+print("➔ Look this special charactere : \"\n")
+
+
+# py 0-basic.py to execute the code
