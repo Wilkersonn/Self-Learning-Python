48c0198106460a1b794675197600ef89f85a8c07
Wilkersonn <remyfournieroff@gmail.com>
2024-08-18 18:57:01 +0200

    Push Project modularity

--- Basic_learning/6-modularity/includes/player.py
+++ Basic_learning/6-modularity/includes/player.py
@@ -12,10 +12,16 @@
 # Module               ➔ player module for main.py
 #                      ➔ to use it type : import includes.player
 #                      ➔ or import includes.player as player
+#                      ➔ if __name__ == "__main__": for testing
 
 def speak(human, message):
     print("{} : {}".format(human, message))
 
 def bye():
     print("Bye !")
+
+if __name__ == "__main__":
+    print("TEST PLAYER.PY\n")
+    speak("HumanTest", "Test of speak fonction")
+    bye()
 
